7c0bcb70145182b0c07972d61c219aa8f9a37549
reduktorius <reduktorius@gmail.com>
2020-05-10 12:44:26 +0100

    @217-220 add error handling, update packages

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,8 @@ import React from 'react';
 function SearchBox({searchChange}) {
     return (
         <div className="pa2">
-            <input className="pa3 ba b--green bg-lightest-blue" type="search" onChange={searchChange} />
+            <input className="pa3 ba b--green bg-lightest-blue" type="search" 
+                    onChange={searchChange} placeholder="Search robots"/>
         </div>
     )
 }
